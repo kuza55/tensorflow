c77bf5a0e5952f64d8d790b99461b33f2b56556f
kuza55 <kuza55@gmail.com>
2016-09-21 12:57:02 -0400

    Maybe starting work on a sparse slice

--- tensorflow/core/util/sparse/sparse_tensor.h
+++ tensorflow/core/util/sparse/sparse_tensor.h
@@ -519,6 +519,74 @@ std::vector<SparseTensor> SparseTensor::Split(const SparseTensor& input_tensor,
   return output_tensors;
 }
 
+template <typename T>
+std::vector<SparseTensor> SparseTensor::Slice(const SparseTensor& input_tensor,
+                                              const Eigen::DSizes<Eigen::DenseIndex, NDIMS>& slice_indices,
+                                              const Eigen::DSizes<Eigen::DenseIndex, NDIMS>& slice_sizes) {
+
+  Tensor output_indices;
+  Tensor output_values;
+  TensorShape output_shape;
+
+  typename TTypes<int64>::Matrix output_indices_t;
+  typename TTypes<T>::Vec output_values_t;
+  auto input_values_t = input_tensor.values().vec<T>();
+  auto input_indices_t = input_tensor.indices().matrix<int64>();
+
+  std::vector<int> num_values(num_split, 0);
+  const int num_dim = input_tensor.shape().dims();
+
+  CHECK(num_split > 0 && num_split <= split_dim_size) << "num_split must be in "
+                                                         "the interval (0, "
+                                                      << split_dim_size << "]";
+  CHECK(split_dim >= 0 && split_dim < num_dim) << "num_dim must be in "
+                                                  "the interval [0, "
+                                               << num_dim << ")";
+
+  
+  for (int i = 0; i < input_tensor.indices().dim_size(0); ++i) {
+    const int dim = input_tensor.indices().matrix<int64>()(i, split_dim);
+    int slice_index = GetSliceIndex(dim, split_size, residual);
+    num_values[slice_index]++;
+  }
+
+  for (int i = 0; i < num_split; ++i) {
+    // TODO(ataei): Pass an allocator to avoid allocating large memory buffer.
+    output_indices.emplace_back(DT_INT64,
+                                TensorShape({num_values[i], num_dim}));
+    output_values.emplace_back(DataTypeToEnum<T>::v(),
+                               TensorShape({num_values[i]}));
+    output_shapes.emplace_back(input_tensor.shape());
+    output_indices_t.emplace_back(output_indices[i].matrix<int64>());
+    output_values_t.emplace_back(output_values[i].vec<T>());
+    const int size = GetSliceShape(i, split_size, residual);
+    output_shapes[i].set_dim(split_dim, size);
+  }
+
+  std::vector<int> values_inserted_in_slice(num_split, 0);
+  for (int i = 0; i < input_tensor.indices().dim_size(0); ++i) {
+    const int dim = input_indices_t(i, split_dim);
+    const int slice_index = GetSliceIndex(dim, split_size, residual);
+    const int slice_dim = values_inserted_in_slice[slice_index]++;
+    output_values_t[slice_index](slice_dim) = input_values_t(i);
+    for (int j = 0; j < num_dim; ++j) {
+      const int64 original_dim = input_indices_t(i, j);
+      output_indices_t[slice_index](slice_dim, j) =
+          (j == split_dim)
+              ? GetDimensionInSlice(original_dim, split_size, residual)
+              : original_dim;
+    }
+  }
+
+  std::vector<SparseTensor> output_tensors;
+  output_tensors.reserve(num_split);
+  for (int i = 0; i < num_split; ++i) {
+    output_tensors.emplace_back(output_indices[i], output_values[i],
+                                output_shapes[i]);
+  }
+  return output_tensors;
+}
+
 }  // namespace sparse
 }  // namespace tensorflow
 
